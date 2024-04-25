e2ce3ec2c62e11e51a4a0f832821279b9c79eeed
DankDar <huzaifa.dar@walee.pk>
2024-04-25 15:08:36 +0500

    reactionOptimized

--- controllers/reactions.js
+++ controllers/reactions.js
@@ -6,46 +6,32 @@ exports.addReaction = async (req, res, next) => {
     try {
         const { postId, type, createdBy } = req.body;
 
-        //
         const post = await Post.findById(postId);
         if (!post) {
             return res.status(404).json({
                 data: response.error('Post not found', 404)
             });
         }
-        if (!post.likes.includes(createdBy)) {
-            // post.likes = post.likes.filter(id => !id.equals(createdBy));
-        // }
-        // else{
+
+        const reaction = await Reaction.findOneAndUpdate(
+            { post: postId, createdBy },
+            { type },
+            { new: true, upsert: true, setDefaultsOnInsert: true }
+        );
+
+
+        if (reaction.isNew && !post.likes.includes(createdBy)) {
             post.likes.push(createdBy);
+            await post.save();
         }
 
-        await post.save();
-        ///
-
-        const existingReaction = await Reaction.findOne({ post: postId, createdBy });
-        if (existingReaction) {
-            existingReaction.type = type;
-            await existingReaction.save();
-
-            return res.status(200).json({
-                data: response.success('Post reacted to successfully', existingReaction, 200)
-            });
-        }
-
-        const reaction = new Reaction({
-            post: postId,
-            type,
-            createdBy
-        });
-        await reaction.save();
-        res.status(201).json({
-            data: response.success('Reacted successfully', reaction, 201)
+        return res.status(reaction.isNew ? 201 : 200).json({
+            data: response.success(reaction.isNew ? 'Reacted successfully' : 'Post reacted to successfully', reaction, reaction.isNew ? 201 : 200)
         });
     }
     catch (error) {
-        return res.status(400).json({
-            data: response.error(error, 400)
+        return res.status(500).json({
+            data: response.error(error, 500)
         });
     }
 };
@@ -54,38 +40,32 @@ exports.removeReaction = async (req, res, next) => {
     try {
         const { reactionId } = req.params;
 
-
-        const reaction = await Reaction.find({
-            _id: reactionId
-        });
-
+        const reaction = await Reaction.findById(reactionId);
+        console.log(reaction);
         if (!reaction) {
             return res.status(404).json({
                 data: response.error('Reaction not found or user not authorized', 404)
             });
         }
 
-        //
-        const post = await Post.findById(reaction[0]?.post);
+        const post = await Post.findById(reaction.post);
         if (!post) {
             return res.status(404).json({
                 data: response.error('Post not found', 404)
             });
         }
-        post.likes = post.likes.filter(id => !id.equals(reaction[0].createdBy));
-        await post.save();
-        ///
 
-        const unreact = await Reaction.findOneAndDelete({
-            _id: reactionId
-        });
+        if (post.likes.some(id => id.equals(reaction.createdBy))) {
+            post.likes = post.likes.filter(id => !id.equals(reaction.createdBy));
+            await post.save();
+        }
 
-        // res.status(200).send({ message: 'Reaction removed' });
+        const unreact = await Reaction.findByIdAndDelete(reactionId);
+
         res.status(200).json({
             data: response.success('Reaction removed', unreact, 200)
         });
-    }
-    catch (error) {
+    } catch (error) {
         return res.status(500).json({
             data: response.error(error, 500)
         });
